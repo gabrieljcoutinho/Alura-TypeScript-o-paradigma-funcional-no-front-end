d2c44ee4e5f4f93b95359d32ac5611afb38108f0
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 10:16:35 -0300

    ul

--- script-crudo.ts
+++ script-crudo.ts
@@ -37,6 +37,8 @@ const selecionarTarefa = (estado: EstadoDaAplicacao, tarefa: Tarefa) : EstadoDaA
 }
 
 const atualizarUI = () => {
-    const ulTarefas = document.querySelector('app__section-task-list"')
-    ulTarefas?.innerHTML = ''
+    const ulTarefas = document.querySelector('.app__section-task-list"')
+    if(ulTarefas){
+            ulTarefas?.innerHTML = ''
+    }
 }
